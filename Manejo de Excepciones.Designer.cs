f8edce00bf19354bb55dde47d06f13266972c14d
estarlingdavidsantanasantana <estarlingdavidsantanasantana@gmail.com>
2026-05-31 17:13:50 -0400

    Nuevo Repositorio

---
+++ Manejo de Excepciones.Designer.cs
@@ -0,0 +1,234 @@
+﻿namespace RegistroEstudiantes
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelNombre = new System.Windows.Forms.Label();
+            this.textBoxNombre = new System.Windows.Forms.TextBox();
+            this.labelEdad = new System.Windows.Forms.Label();
+            this.labelGmail = new System.Windows.Forms.Label();
+            this.labelTelefono = new System.Windows.Forms.Label();
+            this.textBoxEdad = new System.Windows.Forms.TextBox();
+            this.textBoxGmail = new System.Windows.Forms.TextBox();
+            this.textBoxTelefono = new System.Windows.Forms.TextBox();
+            this.buttonGuardar = new System.Windows.Forms.Button();
+            this.buttonLimpiar = new System.Windows.Forms.Button();
+            this.buttonSalir = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label5 = new System.Windows.Forms.Label();
+            this.labelIngresar = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // labelNombre
+            // 
+            this.labelNombre.AutoSize = true;
+            this.labelNombre.Location = new System.Drawing.Point(416, 196);
+            this.labelNombre.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.labelNombre.Name = "labelNombre";
+            this.labelNombre.Size = new System.Drawing.Size(192, 25);
+            this.labelNombre.TabIndex = 0;
+            this.labelNombre.Text = "Nombre Completo:";
+            // 
+            // textBoxNombre
+            // 
+            this.textBoxNombre.Location = new System.Drawing.Point(627, 193);
+            this.textBoxNombre.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.textBoxNombre.Name = "textBoxNombre";
+            this.textBoxNombre.Size = new System.Drawing.Size(303, 30);
+            this.textBoxNombre.TabIndex = 1;
+            // 
+            // labelEdad
+            // 
+            this.labelEdad.AutoSize = true;
+            this.labelEdad.Location = new System.Drawing.Point(416, 291);
+            this.labelEdad.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.labelEdad.Name = "labelEdad";
+            this.labelEdad.Size = new System.Drawing.Size(69, 25);
+            this.labelEdad.TabIndex = 2;
+            this.labelEdad.Text = "Edad:";
+            // 
+            // labelGmail
+            // 
+            this.labelGmail.AutoSize = true;
+            this.labelGmail.Location = new System.Drawing.Point(416, 384);
+            this.labelGmail.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.labelGmail.Name = "labelGmail";
+            this.labelGmail.Size = new System.Drawing.Size(74, 25);
+            this.labelGmail.TabIndex = 3;
+            this.labelGmail.Text = "Gmail:";
+            // 
+            // labelTelefono
+            // 
+            this.labelTelefono.AutoSize = true;
+            this.labelTelefono.Location = new System.Drawing.Point(416, 464);
+            this.labelTelefono.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.labelTelefono.Name = "labelTelefono";
+            this.labelTelefono.Size = new System.Drawing.Size(104, 25);
+            this.labelTelefono.TabIndex = 4;
+            this.labelTelefono.Text = "Telefono:";
+            // 
+            // textBoxEdad
+            // 
+            this.textBoxEdad.Location = new System.Drawing.Point(543, 288);
+            this.textBoxEdad.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.textBoxEdad.Name = "textBoxEdad";
+            this.textBoxEdad.Size = new System.Drawing.Size(313, 30);
+            this.textBoxEdad.TabIndex = 5;
+            // 
+            // textBoxGmail
+            // 
+            this.textBoxGmail.Location = new System.Drawing.Point(543, 379);
+            this.textBoxGmail.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.textBoxGmail.Name = "textBoxGmail";
+            this.textBoxGmail.Size = new System.Drawing.Size(313, 30);
+            this.textBoxGmail.TabIndex = 6;
+            // 
+            // textBoxTelefono
+            // 
+            this.textBoxTelefono.Location = new System.Drawing.Point(552, 459);
+            this.textBoxTelefono.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.textBoxTelefono.Name = "textBoxTelefono";
+            this.textBoxTelefono.Size = new System.Drawing.Size(313, 30);
+            this.textBoxTelefono.TabIndex = 7;
+            // 
+            // buttonGuardar
+            // 
+            this.buttonGuardar.Location = new System.Drawing.Point(322, 605);
+            this.buttonGuardar.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.buttonGuardar.Name = "buttonGuardar";
+            this.buttonGuardar.Size = new System.Drawing.Size(148, 42);
+            this.buttonGuardar.TabIndex = 8;
+            this.buttonGuardar.Text = "Guardar";
+            this.buttonGuardar.UseVisualStyleBackColor = true;
+            this.buttonGuardar.Click += new System.EventHandler(this.buttonGuardar_Click);
+            // 
+            // buttonLimpiar
+            // 
+            this.buttonLimpiar.Location = new System.Drawing.Point(657, 605);
+            this.buttonLimpiar.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.buttonLimpiar.Name = "buttonLimpiar";
+            this.buttonLimpiar.Size = new System.Drawing.Size(148, 42);
+            this.buttonLimpiar.TabIndex = 9;
+            this.buttonLimpiar.Text = "Limpiar";
+            this.buttonLimpiar.UseVisualStyleBackColor = true;
+            this.buttonLimpiar.Click += new System.EventHandler(this.buttonLimpiar_Click);
+            // 
+            // buttonSalir
+            // 
+            this.buttonSalir.Location = new System.Drawing.Point(1027, 605);
+            this.buttonSalir.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.buttonSalir.Name = "buttonSalir";
+            this.buttonSalir.Size = new System.Drawing.Size(148, 42);
+            this.buttonSalir.TabIndex = 10;
+            this.buttonSalir.Text = "Salir";
+            this.buttonSalir.UseVisualStyleBackColor = true;
+            this.buttonSalir.Click += new System.EventHandler(this.buttonSalir_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Red;
+            this.panel1.Controls.Add(this.labelIngresar);
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1350, 146);
+            this.panel1.TabIndex = 11;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(509, 43);
+            this.label5.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(431, 40);
+            this.label5.TabIndex = 0;
+            this.label5.Text = "Resgistro del Estudiante";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // labelIngresar
+            // 
+            this.labelIngresar.AutoSize = true;
+            this.labelIngresar.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelIngresar.Location = new System.Drawing.Point(511, 102);
+            this.labelIngresar.Name = "labelIngresar";
+            this.labelIngresar.Size = new System.Drawing.Size(419, 26);
+            this.labelIngresar.TabIndex = 1;
+            this.labelIngresar.Text = " Ingresar la información del estudiante";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1350, 691);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.buttonSalir);
+            this.Controls.Add(this.buttonLimpiar);
+            this.Controls.Add(this.buttonGuardar);
+            this.Controls.Add(this.textBoxTelefono);
+            this.Controls.Add(this.textBoxGmail);
+            this.Controls.Add(this.textBoxEdad);
+            this.Controls.Add(this.labelTelefono);
+            this.Controls.Add(this.labelGmail);
+            this.Controls.Add(this.labelEdad);
+            this.Controls.Add(this.textBoxNombre);
+            this.Controls.Add(this.labelNombre);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelNombre;
+        private System.Windows.Forms.TextBox textBoxNombre;
+        private System.Windows.Forms.Label labelEdad;
+        private System.Windows.Forms.Label labelGmail;
+        private System.Windows.Forms.Label labelTelefono;
+        private System.Windows.Forms.TextBox textBoxEdad;
+        private System.Windows.Forms.TextBox textBoxGmail;
+        private System.Windows.Forms.TextBox textBoxTelefono;
+        private System.Windows.Forms.Button buttonGuardar;
+        private System.Windows.Forms.Button buttonLimpiar;
+        private System.Windows.Forms.Button buttonSalir;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label labelIngresar;
+    }
+}
+
---
+++ Manejo de Excepciones.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RegistroEstudiantes
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonGuardar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (textBoxNombre.Text.Trim() == "")
+                {
+                    throw new Exception("Debe ingresar el nombre");
+                }
+
+                int edad;
+
+                if (!int.TryParse(textBoxEdad.Text, out edad))
+                {
+                    throw new Exception("La edad debe ser numérica");
+                }
+
+                if (!textBoxGmail.Text.Contains("@"))
+                {
+                    throw new Exception("Correo inválido");
+                }
+
+                if (!textBoxGmail.Text.EndsWith("@gmail.com"))
+                {
+                    throw new Exception("Debe ser un correo Gmail");
+                }
+
+                long telefono;
+
+                if (!long.TryParse(textBoxTelefono.Text, out telefono))
+                {
+                    throw new Exception("El teléfono debe contener números");
+                }
+
+                MessageBox.Show("Estudiante registrado correctamente");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void buttonLimpiar_Click(object sender, EventArgs e)
+        {
+            textBoxNombre.Clear();
+            textBoxEdad.Clear();
+            textBoxGmail.Clear();
+            textBoxTelefono.Clear();
+
+            textBoxNombre.Focus();
+        }
+
+        private void buttonSalir_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
